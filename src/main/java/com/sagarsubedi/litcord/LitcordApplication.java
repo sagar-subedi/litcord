f2aebb580a3016b29e294ac17a765b588e579dca
Sagar Subedi <sagarxic@gmail.com>
2024-09-12 00:13:49 +0545

    first commit

---
+++ src/main/java/com/sagarsubedi/litcord/LitcordApplication.java
@@ -0,0 +1,13 @@
+package com.sagarsubedi.litcord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitcordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitcordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagarsubedi/litcord/LitcordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagarsubedi.litcord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitcordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
