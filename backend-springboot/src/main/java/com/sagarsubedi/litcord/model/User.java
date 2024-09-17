0c45a391a48cc7815ae169ad9e3690d55ac6c615
Sagar Subedi <sagarxic@gmail.com>
2024-09-18 00:32:00 +0545

    1-sagar-subedi User entity created

--- backend-springboot/src/main/java/com/sagarsubedi/litcord/model/User.java
+++ backend-springboot/src/main/java/com/sagarsubedi/litcord/model/User.java
@@ -1,4 +1,31 @@
 package com.sagarsubedi.litcord.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    private Long userId;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    public User(String name,String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return  email;
+    }
 }
