cb80fda06051a9dea5c138f64008c9c3adebb8ab
Sagar Subedi <sagarxic@gmail.com>
2024-10-22 10:01:40 +0545

    6-setup-homepage Added components, guards, and db.json for mocking api calls

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,26 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CallingComponent } from '../calling-component/calling-component.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CallComponent } from './components/call/call.component';
+import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CallingComponent, HttpClientModule],
+  imports: [RouterOutlet, CallComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'litcord';
   apiUrl = 'https://192.168.100.190:8081/';
+  private httpClient = provideHttpClient();
 
-  constructor(private httpClient: HttpClient){
+  constructor(){
 
   }
   ngOnInit(): void {
-    this.httpClient.get(this.apiUrl + 'test').subscribe((response)=>{
-      console.log("The response:" + response);
-    },
-  (err)=>{console.log(err)});
+    
   }
 }
