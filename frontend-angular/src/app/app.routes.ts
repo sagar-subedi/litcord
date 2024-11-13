51522a7cf10322c08087e2efac3162ed226e9d6a
Sagar Subedi <sagarxic@gmail.com>
2024-11-13 10:25:46 +0545

    Added a test component to test random things

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HomeGuard } from './guards/home.guard';
+import { TestComponent } from './components/test/test.component';
 
 export const routes: Routes = [
     { path: '', canActivate: [HomeGuard], pathMatch: 'full', component: HomeComponent},   //canActivate will always return false, so always redirection
     {path:'servers/:serverid/channels/:channelid', component: HomeComponent},
     { path: 'servers', redirectTo: '/servers/23/channels/22', pathMatch: 'full' },
-    {path: 'auth', component: AuthComponent}
+    {path: 'auth', component: AuthComponent},
+    {path: 'test', component: TestComponent}
 ];
