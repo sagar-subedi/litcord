baa65dca8ee991c40ddf4913ba4d62060f90304a
Sagar Subedi <sagarxic@gmail.com>
2024-12-03 22:53:31 +0545

    7-setup-webrtc Moved loginDto

---
+++ backend-springboot/src/main/java/com/sagarsubedi/litcord/dto/LoginDTO.java
@@ -0,0 +1,18 @@
+package com.sagarsubedi.litcord.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO {
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    private String email;
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}
