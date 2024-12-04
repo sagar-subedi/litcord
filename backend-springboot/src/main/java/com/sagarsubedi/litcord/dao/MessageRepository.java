6bcdcccc9e97bcf675dbaf979819fd1e973a9f8b
Sagar Subedi <sagarxic@gmail.com>
2024-12-05 00:49:42 +0545

    7-setup-webrtc Added crud services for messages

--- backend-springboot/src/main/java/com/sagarsubedi/litcord/dao/MessageRepository.java
+++ backend-springboot/src/main/java/com/sagarsubedi/litcord/dao/MessageRepository.java
@@ -2,7 +2,14 @@ package com.sagarsubedi.litcord.dao;
 
 import com.sagarsubedi.litcord.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message,Long> {
+    // Get the last 'n' messages for a specific channelId, ordered by messageId descending
+    @Query("SELECT m FROM Message m WHERE m.channelId = :channelId ORDER BY m.messageId DESC")
+    List<Message> findLastNMessagesByChannelId(Long channelId, Pageable pageable);
 
 }
