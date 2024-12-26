0b119c487b39e224da4f8dce7d6060366994399e
Sagar Subedi <sagarxic@gmail.com>
2024-12-26 15:15:42 +0545

    13-channel-server-crud-implementation Prepare the channel component for further features like invite members, manage members and implement deletion logic for channels/server and some basic validation

--- frontend-angular/src/app/pages/home/channel/channel-button/channel-button.component.ts
+++ frontend-angular/src/app/pages/home/channel/channel-button/channel-button.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-channel-button',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, MatIcon],
   templateUrl: './channel-button.component.html',
   styleUrl: './channel-button.component.scss'
 })
@@ -12,5 +14,32 @@ export class ChannelButtonComponent {
 
   @Input() channel!:any;
   @Input() isSelected: boolean = false;
+
+  @Output() deleteChannel = new EventEmitter<number>();
+  @Output() updateChannelData = new EventEmitter<any>();
+
+  isEditFormVisible = false;
+  editChannelData: any = {};
+
+  confirmDeleteChannel(): void {
+    const confirmDelete = confirm(`Are you sure you want to delete the channel "${this.channel.name}"?`);
+    if (confirmDelete) {
+      this.deleteChannel.emit(this.channel.id);
+    }
+  }
+
+  openEditForm(): void {
+    this.isEditFormVisible = true;
+    this.editChannelData = { ...this.channel }; // Copy current channel data
+  }
+
+  closeEditForm(): void {
+    this.isEditFormVisible = false;
+  }
+
+  updateChannel(): void {
+    this.updateChannelData.emit(this.editChannelData);
+    this.closeEditForm();
+  }
 
 }
