d6de09bc4a81f1abbfdbcb23d7ac05f6e5f8692f
Sagar Subedi <sagarxic@gmail.com>
2025-01-05 01:27:03 +0545

    15-text-channels Add message service

---
+++ frontend-angular/src/app/services/message.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Message } from '../types/Message';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  private apiUrl = 'https://localhost:8081/messages';
+
+  constructor(private http: HttpClient) {}
+
+  addMessage(message: Message): Observable<Message> {
+    return this.http.post<Message>(this.apiUrl, message);
+  }
+
+  getMessages(channelId: number, page: number, size: number): Observable<Message[]> {
+    return this.http.get<Message[]>(`${this.apiUrl}/channel/${channelId}`, {
+      params: { page: page.toString(), size: size.toString() }
+    });
+  }
+}
+
