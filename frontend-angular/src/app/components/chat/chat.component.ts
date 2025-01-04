5eb876c1b0fdded48d8cd06392b17ac7d4d4cc68
Sagar Subedi <sagarxic@gmail.com>
2025-01-05 01:26:40 +0545

    15-text-channels Update chat component for better UI and functionality

--- frontend-angular/src/app/components/chat/chat.component.ts
+++ frontend-angular/src/app/components/chat/chat.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Message } from '../../types/Message';
+import { MessageService } from '../../services/message.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-chat',
@@ -10,57 +13,78 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './chat.component.scss',
 })
 export class ChatComponent implements OnInit {
-  messages: { sender: string; text: string }[] = [];
+  @Input() channel: any;
+  messages: Message[] = [];
   messageInput: string = '';
   currentUser: string = 'user1'; // Replace with the actual logged-in user identifier
+  currentUserEmail: string = 'user@gmail.com';
   isLoading: boolean = false;
 
+  @ViewChild('chatContainer', { static: true }) chatContainer!: ElementRef;
+
+  currentPage = 0;
+  pageSize = 20;
+
+  constructor(
+    private messageService: MessageService,
+    private authService: AuthService
+  ) {}
+
   ngOnInit() {
-    this.loadInitialMessages();
+    this.currentUser = this.authService.getCurrentUserId() ?? '';
+    this.currentUserEmail = this.authService.getCurrentUserEmail() ?? '';
+    // this.loadInitialMessages();
+    this.loadMessages();
   }
 
-  loadInitialMessages() {
-    // Simulate loading initial messages
-    this.messages = [
-      { sender: 'user1', text: 'Hi there!' },
-      { sender: 'user2', text: 'Hello! How can I help you?' },
-      { sender: 'user1', text: 'Hi there!' },
-      { sender: 'user2', text: 'Hello! How can I help you?' }, 
-      { sender: 'user1', text: 'Hi there!' },
-      { sender: 'user2', text: 'Hello! How can I help you?' },
-      { sender: 'user1', text: 'Hi there!' },
-      { sender: 'user2', text: 'Hello! How can I help you?' }, 
-      { sender: 'user1', text: 'Hi there!' },
-      { sender: 'user2', text: 'Hello! How can I help you?' }, 
-      { sender: 'user1', text: 'Hi there!' },
-      { sender: 'user2', text: 'Hello! How can I help you?' },
-    ];
-  }
+  loadMessages(): void {
+    if (this.isLoading) return;
 
-  loadOlderMessages() {
     this.isLoading = true;
-    setTimeout(() => {
-      // Simulate older messages being loaded
-      this.messages.unshift(
-        { sender: 'user2', text: 'This is an older message.' },
-        { sender: 'user1', text: 'Older message from me.' }
-      );
-      this.isLoading = false;
-    }, 1000);
+    this.messageService
+      .getMessages(this.channel.id, this.currentPage, this.pageSize)
+      .subscribe((data) => {
+        if (data.length == 0) {
+          this.isLoading = false;
+          return;
+        }
+        const scrollTopBefore = this.chatContainer.nativeElement.scrollTop;
+
+        // Append new messages to the beginning of the array
+        this.messages = [...data.reverse(), ...this.messages];
+        this.currentPage++;
+
+        // Adjust scroll position to maintain the user's current view
+        setTimeout(() => {
+          this.chatContainer.nativeElement.scrollTop = scrollTopBefore + 50; // Slight offset to allow easier scrolling up again
+          this.isLoading = false;
+        });
+      });
   }
 
   onScroll(event: Event) {
     const target = event.target as HTMLElement;
     if (target.scrollTop === 0 && !this.isLoading) {
-      this.loadOlderMessages();
+      this.loadMessages();
     }
   }
 
   sendMessage() {
     if (this.messageInput.trim() !== '') {
-      this.messages.push({ sender: this.currentUser, text: this.messageInput });
+      const message: Message = {
+        content: this.messageInput,
+        senderId: this.currentUser,
+        senderEmail: this.currentUserEmail,
+        channelId: this.channel.id,
+      };
+      this.messages.push(message);
       this.messageInput = '';
       setTimeout(() => this.scrollToBottom(), 0);
+
+      this.messageService.addMessage(message).subscribe((response: any) => {
+        console.log('Message sent:', response);
+        this.messageInput = ''; // Clear input
+      });
     }
   }
 
